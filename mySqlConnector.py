e6e1fa75fbd3ebe8a175a40905b13b76e23318e6
İbrahim Emre Yıldız <iemreis803@gmail.com>
2026-03-27 00:19:47 +0300

    MySQL database bağlantı çalışması

--- mySqlConnector.py
+++ mySqlConnector.py
@@ -10,9 +10,22 @@ u = os.getenv("USER")
 p = os.getenv("PASSWORD")
 
 mydb=mysql.connector.connect(
-    host = h, #192.30.25.45 Normalde bunu bir yerden service hizmeti olarak satın almamız lazım. Onlar tarafından IP adresi verilecektir.
+    host = h , #192.30.25.45 Normalde bunu bir yerden service hizmeti olarak satın almamız lazım. Onlar tarafından IP adresi verilecektir.
     user = u ,# Kullanıcı adı
-    password = p # Server'a giriş şifrem
+    password = p ,# Server'a giriş şifrem
+    database ="mydatabase" #Hangi database ile çalışacaksak onu yazabiliriz eğer birden fazla varsa
 )
 
+mycursor = mydb.cursor() #mycursor objesi oluşturduk komut verebilmek için mysql'e çünkü mydb ile bağladık
+
+mycursor.execute("SHOW DATABASES") # Databaseleri gösterme
+for x in mycursor:
+    print(x)
+
+
+
+mycursor.execute("CREATE DATABASE mydatabase") # Database oluşturma 
+
 print(mydb)
+
+# Genel olarak Script kullanmaya gerek yok IDE üzerinden rahatça yapabiliriz
